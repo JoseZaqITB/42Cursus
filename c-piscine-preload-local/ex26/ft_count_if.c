6555e370b99c59eaad52b837aef7b8ae1aaf39f0
Jose Alejandro Zaquinaula Ramon <jzaquina@cbr11s4.42barcelona.com>
2026-04-20 19:17:30 +0200

    update until ex25, try 26

---
+++ c-piscine-preload-local/ex26/ft_count_if.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzaquina <jzaquina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/20 19:00:54 by jzaquina          #+#    #+#             */
+/*   Updated: 2026/04/20 19:11:28 by jzaquina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+int ft_count_if(char **tab, int (*f)(char*))
+{
+	int	total;
+
+	total = 0;
+	while (tab )
+	if ((*f)(tab) == 1)
+		total++;
+	return (total);
+}
+
+
+int	ft_one(char *str)
+{
+	if (str != "Perro")
+		return (1);
+	return (0);
+}
+
+int	main()
+{
+	char *tabArray[] = ["Gato", "Perro", "Oso"];
+	char **tab = tabArray;
+	ft_count_if(tab, &ft_one);
+}
+*/
