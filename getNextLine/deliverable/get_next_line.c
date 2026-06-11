7a251a21276fd2500ee5cb17ca3f19b344fcb7bb
laptop <yoseyus@gmail.com>
2026-06-11 21:10:43 +0200

    next: get loine with buffersize = 1

--- getNextLine/deliverable/get_next_line.c
+++ getNextLine/deliverable/get_next_line.c
@@ -6,13 +6,12 @@
 /*   By: jzaquina <jzaquina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 16:30:17 by jzaquina          #+#    #+#             */
-/*   Updated: 2026/06/10 21:11:07 by jzaquina         ###   ########.fr       */
+/*   Updated: 2026/06/11 21:09:49 by jzaquina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
 
 int	get_breakline(const char *s)
 {
@@ -26,11 +25,39 @@ int	get_breakline(const char *s)
 		if (*str == '\n')
 			break ;
 		if (!*str)
-			return (0);
+			return (-1);
 		str++;
 		i++;
 	}
 	return (i);
+}
+
+int	readbreakline(int fd, char **s)
+{
+	char		buffer[BUFFER_SIZE];
+	int			breakpos;
+	int			curpos;
+	int			i;
+
+	curpos = 0;
+	breakpos = 0;
+	i = -1;
+	while ((*s)[++i])
+		buffer[i] = (*s)[i];
+	while (1)
+	{
+		curpos = get_breakline(buffer);
+		if (curpos != -1)
+		{
+			printf("%d", curpos);
+			return (breakpos + curpos);
+		}
+		breakpos += ft_strlen(buffer);
+		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+			return (-1);
+		*s = ft_strjoin(*s, buffer);
+	}
+	return (breakpos);
 }
 
 char	*get_next_line(int fd)
@@ -39,7 +66,7 @@ char	*get_next_line(int fd)
 	char		*tempbuffer;
 	char		buffer[BUFFER_SIZE];
 
-	int	breakpos;
+	int		breakpos;
 	char	*line;
 
 	line = 0;
@@ -50,18 +77,18 @@ char	*get_next_line(int fd)
 	// 1. read() / getbreaklinefrombuffer()
 	if (!leftover)
 	{
-		read(fd, buffer, BUFFER_SIZE);
+		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+			return (0);
 		tempbuffer = &buffer[0];
 	}
-	printf("--%s--", tempbuffer);
-	// 2. get_breakline()
-	breakpos = get_breakline(tempbuffer);
-
+	// 2. readbreakline()/get_breakline()
+	breakpos = readbreakline(fd, &tempbuffer);
+	printf("break: %d, tempBuffer: %s", breakpos, tempbuffer);
 	// 3. appendleftover()
 	tempbuffer = ft_strjoin(leftover, tempbuffer);
 	// 5. return_line()
 	line = ft_substr(tempbuffer, 0, breakpos);
 	// 4. saveleftover()
 	leftover = ft_substr(tempbuffer, breakpos + 1, ft_strlen(tempbuffer) - (breakpos + 1));
-	return line;
+	return (line);
 }
