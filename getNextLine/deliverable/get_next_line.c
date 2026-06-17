1a4a15b839a8921e9ca22831136e8ea768d41563
laptop <yoseyus@gmail.com>
2026-06-17 19:05:20 +0200

    next: able to read more than 1 byte by loop

--- getNextLine/deliverable/get_next_line.c
+++ getNextLine/deliverable/get_next_line.c
@@ -6,35 +6,45 @@
 /*   By: jzaquina <jzaquina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 16:30:17 by jzaquina          #+#    #+#             */
-/*   Updated: 2026/06/12 18:05:56 by jzaquina         ###   ########.fr       */
+/*   Updated: 2026/06/17 19:04:09 by jzaquina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+char	*readline(int fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		readcount;
+
+	readcount = read(fd, buffer, BUFFER_SIZE);
+	if (readcount <= 0)
+		return (0);
+	buffer[readcount] = '\0';
+	return (ft_strjoin("", &buffer[0]));
+}
+
 int	get_breakline(const char *s)
 {
 	char	*str;
-	char	**res;
 	int		i;
 
 	str = (char *)s;
+	i = 0;
 	while (1)
 	{
-		if (*str == '\n')
+		if (str[i] == '\n')
 			break ;
-		if (!*str)
+		if (!str[i++])
 			return (-1);
-		str++;
-		i++;
 	}
 	return (i);
 }
 
 int	readbreakline(int fd, char **s)
 {
-	char		buffer[BUFFER_SIZE];
+	char		*buffer;
 	int			breakpos;
 	int			curpos;
 	int			i;
@@ -42,52 +52,44 @@ int	readbreakline(int fd, char **s)
 	curpos = 0;
 	breakpos = 0;
 	i = -1;
-	while ((*s)[++i])
-		buffer[i] = (*s)[i];
+	buffer = *s;
 	while (1)
 	{
 		curpos = get_breakline(buffer);
 		if (curpos != -1)
 			return (breakpos + curpos);
 		breakpos += ft_strlen(buffer);
-		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+		buffer = readline(fd);
+		if (!buffer)
 			return (-1);
 		*s = ft_strjoin(*s, buffer);
 	}
 	return (breakpos);
 }
 
+char	*ft_substr_from(char *s, int start)
+{
+	return (ft_substr(s, start, ft_strlen(s) - start));
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*leftover;
 	char		*tempbuffer;
-	char		buffer[BUFFER_SIZE];
-
-	int		breakpos;
-	char	*line;
+	int			breakpos;
+	char		*line;
 
 	line = 0;
-	// if buffer is not empty, play with it
-	// if not, read a new bunch of bytes and play with them
-	printf("leftover: %s\n", leftover);
-
-	// 1. read() / getbreaklinefrombuffer()
 	if (!leftover || leftover[0] == '\0')
 	{
-		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+		tempbuffer = readline(fd);
+		if (!tempbuffer)
 			return (0);
-		tempbuffer = &buffer[0];
 	}
 	else
 		tempbuffer = leftover;
-	// 2. readbreakline()/get_breakline()
 	breakpos = readbreakline(fd, &tempbuffer);
-	// 3. appendleftover()
-	// 5. return_line()
 	line = ft_substr(tempbuffer, 0, breakpos);
-	// 4. saveleftover()
-	printf("tempbuffer: --%s--\n", tempbuffer);
-	leftover = ft_substr(tempbuffer, breakpos + 1, ft_strlen(tempbuffer) - (breakpos + 1));
-	printf("new leftover: --%s--\n", leftover);
+	leftover = ft_substr_from(tempbuffer, breakpos + 1);
 	return (line);
 }
